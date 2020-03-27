30904089d62262cdcd5257f9beb45c49780a726c
Tobin Yehle <tobinyehle@gmail.com>
2020-03-26 17:48:02 -0700

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="yogaflo",
-    version="0.1.0",
+    version="0.1.1",
     author="Tobin Yehle",
     author_email="tobinyehle@gmail.com",
     description="Yoga Flow Generator",
